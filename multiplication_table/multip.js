4677c864f04a986b06f1e0ee1b68a3355f13ae9d
DolngNothing <1178363215@qq.com>
2020-06-10 11:06:13 +0800

    lzh

---
+++ multiplication_table/multip.js
@@ -0,0 +1,6 @@
+for(var x=1;x<10;x++){
+	for(var y=1;y<=x;y++){
+		document.write(x+"*"+y+"="+x*y+" ");
+	}
+	document.write("<br />");
+}
---
+++ number/number.js
@@ -0,0 +1,8 @@
+for(var x=1;x<=20;x++){
+	if(x%2==0){
+		document.write(x+" is even number."+"<br />");
+	}
+	else{
+		document.write(x+" is odd number."+"<br />");
+	}
+}
